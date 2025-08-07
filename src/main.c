85fc49c00d600ddccaaf941486cf65f40a36197c
Brandon C. Lewis <phoenix.no624@protonmail.com>
2025-08-07 17:01:13 -0500

    Added some code to get temp sensor files.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,47 @@
+#include "lists.h"
 #include "thermals.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    struct SENSOR sensors[SENSOR_MAX];
-    unsigned short count = get_sensors(sensors);
-    int i;
+    struct NODE *sensors = (struct NODE *)malloc(sizeof(struct NODE *));
 
-    for (i = 0; i < count; i++)
-    {
-        printf("Sensor Name: %s\n", sensors[i].name);
-        printf("Sensor Temperature: %3.2f F\n", sensors[i].temperature);
-    }
+    get_sensors(sensors);
 
     return 0;
+}
+
+void linked_list_test(void)
+{
+    struct NODE *base;
+    struct NODE *head;
+
+    base = (struct NODE *)malloc(sizeof(struct NODE));
+    base->data = "Hello, ";
+    base->next = (struct NODE *)malloc(sizeof(struct NODE));
+
+    head = base->next;
+    head->data = "World! ";
+    head->next = (struct NODE *)malloc(sizeof(struct NODE));
+
+    head = head->next;
+    head->data = "How ";
+    head->next = (struct NODE *)malloc(sizeof(struct NODE));
+
+    head = head->next;
+    head->data = "are ";
+    head->next = (struct NODE *)malloc(sizeof(struct NODE));
+
+    head = head->next;
+    head->data = "You?";
+    head->next = (struct NODE *)malloc(sizeof(struct NODE));
+
+    struct NODE *current = base;
+    while (current != NULL)
+    {
+        printf(current->data);
+        current = current->next;
+    }
 }
