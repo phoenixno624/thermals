49564bdd44494d8590e646f110fa91de45f6a206
Brandon C. Lewis <phoenix.no624@protonmail.com>
2025-08-07 14:04:48 -0500

    Improved debugging with CMake and Ninja

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+#include "thermals.h"
+
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    struct SENSOR sensors[SENSOR_MAX];
+    unsigned short count = get_sensors(sensors);
+    int i;
+
+    for (i = 0; i < count; i++)
+    {
+        printf("Sensor Name: %s\n", sensors[i].name);
+        printf("Sensor Temperature: %3.2f F\n", sensors[i].temperature);
+    }
+
     return 0;
 }
